74a521d816af0587c6a0f26317a6ca884482a0ee
jfrosorio <jfrosorio@gmail.com>
2025-12-30 16:38:40 +0000

    feat(delivery receipts): add create form to dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,5 +7,23 @@
         @lang('app.welcome_user', ['name' => auth()->user()->name])
     </div>
 
-    @include('hqannouncements::list')
+    <div class="space-y-6">
+        @include('hqannouncements::list')
+
+        <form action="{{ route('delivery-receipts.store') }}" method="POST" enctype="multipart/form-data">
+            <x-ui.card>
+                <x-slot:heading>
+                    @choice('app.delivery_receipts', 1)
+                </x-slot:heading>
+
+                @include('modules.delivery-receipts.partials.form')
+
+                <x-slot:footer>
+                    <x-button>
+                        @lang('crud.save')
+                    </x-button>
+                </x-slot:footer>
+            </x-ui.card>
+        </form>
+    </div>
 </x-app-layout>
